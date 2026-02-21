a297388df817679958ded21f14acb4bc0f932c8f
MathiyazhaganNTl <programmermathi@gmail.com>
2026-02-22 00:11:02 +0530

    env file update and security check

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsokuB5TMbhSDChK11i8yGOiG61c07E2k",
-    authDomain: "learnsphere-48f5d.firebaseapp.com",
-    databaseURL: "https://learnsphere-48f5d-default-rtdb.firebaseio.com",
-    projectId: "learnsphere-48f5d",
-    storageBucket: "learnsphere-48f5d.firebasestorage.app",
-    messagingSenderId: "338674565830",
-    appId: "1:338674565830:web:afdedf604415de13f00dc7",
-    measurementId: "G-BHMBMEXF67"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
